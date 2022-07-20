8e26803f4e2fcf678d2571468841ba72c38b2abd
John-Arboleda <johnjack0126@gmail.com>
2022-07-20 07:46:23 -0500

    Add react router dom. Render Greeting component

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'static#index'
+  
   resources :greetings, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '#page', to: 'static#index', constraints: =>(req) do
+  #   !req.xhr? && req.format.html?
+  # end
+  
+
+  root 'static#index'
 end
